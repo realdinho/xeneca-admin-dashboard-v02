d10b332ce54dd23c29248874830c66f1704eebef
realdinho <realdo.dias3@gmail.com>
2024-01-30 00:11:33 +0800

    add auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { EffectsModule } from '@ngrx/effects';
-
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
